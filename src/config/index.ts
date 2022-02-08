4964307b5f96852cb4f49a1e523974d01b7ac592
Kat Schelonka <kschelonka@mozilla.com>
2022-02-07 17:23:52 -0800

    fix(setup): fix codebuild and deployment

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,10 +3,28 @@ export default {
   app: {
     environment: process.env.NODE_ENV || 'development',
     defaultMaxAge: 86400,
+    port: 4008,
   },
   sentry: {
     dsn: process.env.SENTRY_DSN || '',
     release: process.env.GIT_SHA || '',
     environment: process.env.NODE_ENV || 'development',
+  },
+  database: {
+    // contains tables for user, list, tags, annotations, etc.
+    read: {
+      host: process.env.DATABASE_READ_HOST || 'localhost',
+      port: process.env.DATABASE_READ_PORT || '3309',
+      user: process.env.DATABASE_READ_USER || 'root',
+      password: process.env.DATABASE_READ_PASSWORD || '',
+    },
+    write: {
+      host: process.env.DATABASE_WRITE_HOST || 'localhost',
+      port: process.env.DATABASE_WRITE_PORT || '3309',
+      user: process.env.DATABASE_WRITE_USER || 'root',
+      password: process.env.DATABASE_WRITE_PASSWORD || '',
+    },
+    dbName: process.env.DATABASE || 'readitla_ril-tmp',
+    tz: process.env.DATABASE_TZ || 'US/Central',
   },
 };
