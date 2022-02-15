2f817f0c09b36bf7e3e760d9139ba30febac877d
Anthony Liddle <anthonyliddle@gmail.com>
2022-02-15 10:14:02 -0800

    feat(update): adding update highlight mutation [INFRA-287] (#24)
    * feat: Update server to have per-request context
    
    Add local database tables
    
    Create read/write db connections using Knex
    
    Create types for graphql schema and db user_annotations entity
    
    * feat(highlights): scaffolding out dataservice + integration tests
    
    * test: pull data into separate file
    
    * chore: port cleanups
    
    * test: add deleted highlight to test fixture
    
    * feat(highlights): Start resolver implementation
    
    Pairing with @kkyeboah @sri-kirsh @collectedmind
    @anthony-liddle @wtfluckely
    
    * test: fix test issue and add list query
    
    * feat(highlights): boilerplate code for highlights CRUD mutations
    
    Update schema
    
    Add types from schema
    
    Add helper function for converting from input
    type to database entity
    
    * feat(highlights): add premium status to context
    
    * fix: isPremium readonly
    
    * test: scaffold update highlights mutation
    
    * added update highlight mutation
    
    * fix(update): finish update mutation tests and functions
    
    * chore: lint
    
    * chore: lint
    
    * Update src/test/mutation/highlights-update.integration.ts
    
    * fix(feedback): updating code per PR feedback
    
    Co-authored-by: Kat Schelonka <kschelonka@mozilla.com>
    Co-authored-by: Kat Schelonka <34227334+kschelonka@users.noreply.github.com>

--- src/test/mutation/highlights-mutations.ts
+++ src/test/mutation/highlights-mutations.ts
@@ -1,5 +1,17 @@
 import { gql } from 'apollo-server-core';
 
+export const UPDATE_HIGHLIGHT = gql`
+  mutation updateSavedItemHighlight($id: ID!, $input: CreateHighlightInput!) {
+    updateSavedItemHighlight(id: $id, input: $input) {
+      id
+      patch
+      version
+      quote
+      _createdAt
+      _updatedAt
+    }
+  }
+`;
 export const DELETE_HIGHLIGHT = gql`
   mutation DeleteHighlight($id: ID!) {
     deleteSavedItemHighlight(id: $id)
---
+++ src/test/mutation/highlights-update.integration.ts
@@ -0,0 +1,115 @@
+import { ApolloServer } from 'apollo-server-express';
+import { getServer } from '../../server';
+import sinon from 'sinon';
+import { ContextManager } from '../../context';
+import { readClient } from '../../database/client';
+import { seedData } from '../query/highlights-fixtures';
+import { UPDATE_HIGHLIGHT } from './highlights-mutations';
+import { HighlightEntity, HighlightInput } from '../../types';
+
+describe('Highlights update', () => {
+  let server: ApolloServer;
+  let contextStub;
+  const userId = 1;
+  const db = readClient();
+  const now = new Date();
+  const testData = seedData(now);
+  const truncateAndSeed = async () => {
+    await Promise.all(
+      Object.keys(testData).map((table) => db(table).truncate())
+    );
+    await Promise.all(
+      Object.entries(testData).map(([table, data]) => db(table).insert(data))
+    );
+  };
+  beforeAll(async () => {
+    contextStub = sinon.stub(ContextManager.prototype, 'userId').value(userId);
+    server = getServer();
+  });
+  afterAll(() => {
+    contextStub.restore();
+  });
+  beforeEach(async () => {
+    await truncateAndSeed();
+  });
+  it('should update an existing highlight owned by the user', async () => {
+    const input = {
+      itemId: '1',
+      version: 2,
+      patch: 'Prow scuttle parrel',
+      quote: 'provost Sail ho shrouds spirits boom',
+    };
+    const id = '1';
+    const variables: { id: string; input: HighlightInput } = {
+      id,
+      input,
+    };
+    const res = await server.executeOperation({
+      query: UPDATE_HIGHLIGHT,
+      variables,
+    });
+
+    expect(res?.data?.updateSavedItemHighlight).toBeTruthy();
+    expect(res?.data?.updateSavedItemHighlight.patch).toEqual(input.patch);
+    expect(res?.data?.updateSavedItemHighlight.quote).toEqual(input.quote);
+    expect(res?.data?.updateSavedItemHighlight.version).toEqual(input.version);
+    expect(res?.data?.updateSavedItemHighlight.id).toEqual(id);
+  });
+  it('should throw a NOT_FOUND error if the annotation_id does not exist', async () => {
+    const variables: { id: string; input: HighlightInput } = {
+      id: '999',
+      input: {
+        itemId: '1',
+        version: 2,
+        patch: 'Prow scuttle parrel',
+        quote: 'provost Sail ho shrouds spirits boom',
+      },
+    };
+    const res = await server.executeOperation({
+      query: UPDATE_HIGHLIGHT,
+      variables,
+    });
+
+    expect(res?.errors?.[0]?.message).toContain(
+      'Error - Not Found: No annotation found for the given ID'
+    );
+
+    // this is really supposed to throw a NOT_FOUND error but in a test env it throws INTERNAL_SERVER_ERROR
+    // expect(res?.errors?.[0]?.extensions?.code).toEqual('NOT_FOUND');
+  });
+  it('should throw a NOT_FOUND error if the annotation_id is not owned by the user, and not update', async () => {
+    await db('user_annotations').insert({
+      annotation_id: 55,
+      user_id: 2,
+      item_id: 2,
+      patch: 'Prow scuttle parrel',
+      quote: 'provost Sail ho shrouds spirits boom',
+      version: 1,
+      status: 1,
+      updated_at: now,
+      created_at: now,
+    });
+    const variables: { id: string; input: HighlightInput } = {
+      id: '55',
+      input: {
+        itemId: '2',
+        version: 2,
+        patch: 'wherry doubloon chase',
+        quote: 'Belay yo-ho-ho keelhaul squiffy black spot',
+      },
+    };
+    const res = await server.executeOperation({
+      query: UPDATE_HIGHLIGHT,
+      variables,
+    });
+
+    const dbRow = await db<HighlightEntity>('user_annotations')
+      .select()
+      .where('annotation_id', variables.id);
+
+    expect(dbRow.length).toEqual(1);
+    expect(res?.errors?.[0].message).toContain(
+      'Error - Not Found: No annotation found for the given ID'
+    );
+  });
+});
