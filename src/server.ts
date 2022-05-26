87dbe08af6b234198bb533d65c7c51a97c0cff69
Daniel Brooks <bass_rock@me.com>
2022-05-26 10:58:26 -0700

    chore(deps): upgrading dependencies (#191)
    * chore(pin): pin deps
    
    * fix(apollo): upgrading apollo library
    
    * fix(csrf): adding the csrf prevention
    
    * fix(aws): tslint in aws
    
    * fix(packages): updating packages
    
    Co-authored-by: Kat Schelonka <34227334+kschelonka@users.noreply.github.com>

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from 'apollo-server-express';
 import typeDefs from './typeDefs';
 import { resolvers } from './resolvers';
-import { buildSubgraphSchema } from '@apollo/federation';
+import { buildSubgraphSchema } from '@apollo/subgraph';
 import { sentryPlugin } from '@pocket-tools/apollo-utils';
 import {
   ApolloServerPluginLandingPageGraphQLPlayground,
@@ -14,6 +14,7 @@ import { dynamoClient, readClient, writeClient } from './database/client';
 
 export function getServer(): ApolloServer {
   return new ApolloServer({
+    csrfPrevention: true,
     schema: buildSubgraphSchema([{ typeDefs, resolvers }]),
     plugins: [
       sentryPlugin,
