94dac4502a3d41892ad1fbfd47dcb08f43ec4b18
Katerina Chinnappan <katchinnapp@gmail.com>
2023-04-20 14:30:23 -0400

    CHORES: CircleCI update (#474)
    * CHORES: CircleCI update
    
    * update to node 18 in package.json
    
    * update AWS infra
    
    * update nvmrc to use node18
    
    * updating tf modules

--- .aws/src/SqsLambda.ts
+++ .aws/src/SqsLambda.ts
@@ -1,11 +1,14 @@
 import { Resource } from 'cdktf';
 import { Construct } from 'constructs';
 import { config } from './config';
-import { PocketVPC } from '@pocket-tools/terraform-modules';
-import { PocketSQSWithLambdaTarget } from '@pocket-tools/terraform-modules';
-import { LAMBDA_RUNTIMES } from '@pocket-tools/terraform-modules';
-import { ssm } from '@cdktf/provider-aws';
-import { PocketPagerDuty } from '@pocket-tools/terraform-modules';
+import {
+  LAMBDA_RUNTIMES,
+  PocketPagerDuty,
+  PocketSQSWithLambdaTarget,
+  PocketVPC,
+} from '@pocket-tools/terraform-modules';
+import { DataAwsRegion } from '@cdktf/provider-aws/lib/data-aws-region';
+import { DataAwsCallerIdentity } from '@cdktf/provider-aws/lib/data-aws-caller-identity';
 
 export class SqsLambda extends Resource {
   public readonly lambda: PocketSQSWithLambdaTarget;
@@ -14,11 +17,11 @@ export class SqsLambda extends Resource {
     scope: Construct,
     private name: string,
     vpc: PocketVPC,
+    region: DataAwsRegion,
+    caller: DataAwsCallerIdentity,
     pagerDuty?: PocketPagerDuty
   ) {
     super(scope, name);
-
-    const { sentryDsn, gitSha } = this.getEnvVariableValues();
 
     this.lambda = new PocketSQSWithLambdaTarget(this, 'sqs-event-consumer', {
       name: `${config.prefix}-Sqs-Event-Consumer`,
@@ -35,8 +38,8 @@ export class SqsLambda extends Resource {
         timeout: 120,
         reservedConcurrencyLimit: config.reservedConcurrencyLimit,
         environment: {
-          SENTRY_DSN: sentryDsn,
-          GIT_SHA: gitSha,
+          SENTRY_DSN: `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}/SENTRY_DSN`,
+          GIT_SHA: `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}/SERVICE_HASH`,
           ENVIRONMENT:
             config.environment === 'Prod' ? 'production' : 'development',
           ANNOTATIONS_API_URI:
@@ -68,17 +71,5 @@ export class SqsLambda extends Resource {
       },
       tags: config.tags,
     });
-  }
-
-  private getEnvVariableValues() {
-    const sentryDsn = new ssm.DataAwsSsmParameter(this, 'sentry-dsn', {
-      name: `/${config.name}/${config.environment}/SENTRY_DSN`,
-    });
-
-    const serviceHash = new ssm.DataAwsSsmParameter(this, 'service-hash', {
-      name: `${config.circleCIPrefix}/SERVICE_HASH`,
-    });
-
-    return { sentryDsn: sentryDsn.value, gitSha: serviceHash.value };
   }
 }
